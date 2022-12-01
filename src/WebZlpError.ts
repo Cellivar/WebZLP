9e0e85656fe5c40d5f53f7ecfe6dc257c8d5b92c
Cellivar <1441553+Cellivar@users.noreply.github.com>
2022-12-01 05:23:21 -0500

    Add utility classes other things build on

---
+++ src/WebZlpError.ts
@@ -0,0 +1,7 @@
+/** Exception thrown from the WebZLP library. */
+export class WebZlpError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = this.constructor.name;
+    }
+}
