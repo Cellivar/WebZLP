2fa52ea9297986cc239aad0d5bdaff203c6cc7e5
Cellivar <1441553+Cellivar@users.noreply.github.com>
2022-12-01 05:33:32 -0500

    Update demo with new lib

--- demo/ts-browser.js
+++ demo/ts-browser.js
@@ -1,61 +1,54 @@
 // https://github.com/harrysolovay/ts-browser
+// and
+// https://github.com/xitu/inline-module
+// and
+// https://glitch.com/edit/#!/typescript-in-browser?path=service-worker.js%3A15%3A0
+// mooshed together until they worked.
 
 const tsTranspiledEvent = new Event('tsTranspiled');
 
-const workerFile = window.URL.createObjectURL(
-  new Blob(
-    [
-      `
-        importScripts('https://unpkg.com/typescript@latest')
-        const load = sourceUrl => {
-          const xhr = XMLHttpRequest
-            ? new XMLHttpRequest()
-            : ActiveXObject
-            ? new ActiveXObject('Microsoft.XMLHTTP')
-            : null
-          if (!xhr) return ''
-          xhr.open('GET', sourceUrl, false)
-          xhr.overrideMimeType && xhr.overrideMimeType('text/plain')
-          xhr.send(null)
-          return xhr.status == 200 ? xhr.responseText : ''
-        }
-        onmessage = ({data: [sourceUrl, sourceCode]}) => {
-          const raw = sourceCode ? sourceCode : load(sourceUrl)
-          const transpiled = ts.transpile(raw)
-          postMessage(transpiled)
-        }
-      `
-    ],
-    { type: 'text/javascript' }
-  )
-);
+function createBlob(code, type = 'text/plain') {
+  const blob = new Blob([code], {type});
+  const blobURL = URL.createObjectURL(blob);
+  return blobURL;
+}
 
 window.addEventListener('DOMContentLoaded', async () => {
   const scripts = document.getElementsByTagName('script');
-  let pending = [];
 
-  for (let i = 0; i < scripts.length; i++) {
-    if (scripts[i].type === 'text/typescript') {
-      const { src } = scripts[i];
-      const innerHtml = src ? null : scripts[i].innerHTML;
+  // Register the Service Worker which will polyfill the HTML module behavior.
+  await navigator.serviceWorker.register('../demo-sw.js');
 
-      pending.push(
-        new Promise((resolve) => {
-          const w = new Worker(workerFile);
-          w.postMessage([src, innerHtml]);
-          w.onmessage = ({ data: transpiled }) => {
-            const newScript = document.createElement('script');
-            newScript.innerHTML = `window.addEventListener('tsTranspiled', function() {
-              ${transpiled}
-            })`;
-            scripts[i].replaceWith(newScript);
-            resolve();
-          };
-        })
-      );
+  // Unfortunately, we have to wait for the Service Worker to ready before
+  // actually loading the application so that it can intercept the HTML requests.
+  await navigator.serviceWorker.ready.then(async worker => {
+
+    // Next up is to compile the inline typescript present on the page.
+    // Pick up their contents, yeet them at the compiler, and then load the result as a URL blob
+    // so that modules will load properly.
+    let pending = [];
+    for (let i = 0; i < scripts.length; i++) {
+      if (scripts[i].type === 'text/typescript') {
+        pending.push(
+          new Promise(resolve => {
+            worker.active.postMessage([`Inline script tag ${i}`, scripts[i].innerHTML]);
+
+            navigator.serviceWorker.onmessage = async ({ data: transpiled }) => {
+              // In order for the browser to treat this as the es6 module it is
+              // we must trick it into 'loading' it. We encode it into a blob URL
+              // and then 'import' that.
+
+              // TODO: Post it externally and then load it inline so it looks more normal?
+              var scriptAsBlob = createBlob(transpiled, 'text/javascript')
+              await import(scriptAsBlob);
+              resolve();
+            }
+          }),
+        )
+      }
     }
-  }
 
-  await Promise.all(pending);
-  window.dispatchEvent(tsTranspiledEvent);
+    await Promise.all(pending);
+    window.dispatchEvent(tsTranspiledEvent);
+  });
 });
