9b3289999c9544411490c8aeae1d03210f6b02bc
Cellivar <cellivar.kangaroorat@gmail.com>
2022-12-29 05:16:10 -0800

    Make tests run faster-er

--- jest.config.ts
+++ jest.config.ts
@@ -3,14 +3,12 @@ import type { Config } from 'jest';
 export default async (): Promise<Config> => {
   return {
     preset: 'ts-jest',
-    extensionsToTreatAsEsm: ['.ts'],
+    extensionsToTreatAsEsm: ['.ts', '.tsx'],
+    transform: {
+      '^.+\\.(t|j)sx?$': '@swc/jest'
+    },
     moduleNameMapper: {
       '^(\\.{1,2}/.*)\\.js$': '$1'
-    },
-    globals: {
-      'ts-jest': {
-        useESM: true
-      }
     },
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
