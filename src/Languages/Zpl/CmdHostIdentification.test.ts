e66fcd6e7fe35ea2165b7f35b1ea6b44958e9d1a
Cellivar <1441553+Cellivar@users.noreply.github.com>
2024-12-30 05:49:06 -0800

    Add tests for the weird host ident formats..

--- src/Languages/Zpl/CmdHostIdentification.test.ts
+++ src/Languages/Zpl/CmdHostIdentification.test.ts
@@ -3,19 +3,65 @@ import { AsciiCodeStrings } from '../../Util/ASCII.js';
 import { CmdHostIdentification, parseCmdHostIdentification } from './CmdHostIdentification.js';
 
 describe('parseCmdHostIdentification', () => {
-  it('Parses a valid message', () => {
+  const cmd = new CmdHostIdentification();
+
+  it('ZD410 Standard', () => {
+    const msg = `${AsciiCodeStrings.STX}ZD410-200dpi,V84.20.18Z,8,8192KB${AsciiCodeStrings.ETX}\r\n`;
+    expect(parseCmdHostIdentification(msg, cmd)).toMatchInlineSnapshot(`
+      {
+        "messageIncomplete": false,
+        "messageMatchedExpectedCommand": true,
+        "messages": [
+          {
+            "messageType": "SettingUpdateMessage",
+            "printerHardware": {
+              "firmware": "V84.20.18Z",
+            },
+            "printerMedia": {},
+          },
+        ],
+        "remainder": "",
+      }
+    `);
+  });
+
+  it('LP2844-Z Standard', () => {
     const msg = `${AsciiCodeStrings.STX}LP2844-Z,V45.11.7Z   ,8,8192KB${AsciiCodeStrings.ETX}\r\n`;
-    expect(parseCmdHostIdentification(msg, new CmdHostIdentification())).toEqual({
-      messageIncomplete: false,
-      messageMatchedExpectedCommand: true,
-      messages: [{
-        messageType: 'SettingUpdateMessage',
-        printerHardware: {
-          firmware: "V45.11.7Z"
-        },
-        printerMedia: {}
-      }],
-      remainder: "",
-    });
+    expect(parseCmdHostIdentification(msg, cmd)).toMatchInlineSnapshot(`
+      {
+        "messageIncomplete": false,
+        "messageMatchedExpectedCommand": true,
+        "messages": [
+          {
+            "messageType": "SettingUpdateMessage",
+            "printerHardware": {
+              "firmware": "V45.11.7Z",
+            },
+            "printerMedia": {},
+          },
+        ],
+        "remainder": "",
+      }
+    `);
+  });
+
+  it('ZP505 Standard', () => {
+    const msg = `${AsciiCodeStrings.STX}ZP 500 (ZPL)-200dpi,ZSP-002281B,8,2104KB${AsciiCodeStrings.ETX}\r\n`;
+    expect(parseCmdHostIdentification(msg, cmd)).toMatchInlineSnapshot(`
+      {
+        "messageIncomplete": false,
+        "messageMatchedExpectedCommand": true,
+        "messages": [
+          {
+            "messageType": "SettingUpdateMessage",
+            "printerHardware": {
+              "firmware": "ZSP-002281B",
+            },
+            "printerMedia": {},
+          },
+        ],
+        "remainder": "",
+      }
+    `);
   });
 });
